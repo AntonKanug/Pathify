a8066c9d352d9d5f940fbc937865e14d8b37793e
Anton Kanugalawattage <antondilon@gmail.com>
2020-02-11 07:03:08 -0500

    app comp

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,26 +1,13 @@
 import React, { Component } from 'react'
-import ReactMapGL from 'react-map-gl';
-
+import Map from './Map'
+import Search from './Search'
 export class App extends Component {
-    state= {
-        viewport : {
-            width: "100%",
-            height: "100vh",
-            latitude: 37.7577,
-            longitude: -122.4376,
-            zoom: 8
-        }
-    }
     render() {
 
         return (
             <div>
-                <ReactMapGL
-                    {...this.state.viewport}
-                    mapStyle={'mapbox://styles/antondilon/ck6f57y7g7irc1inwhihl51yp'}
-                    mapboxApiAccessToken={"pk.eyJ1IjoiYW50b25kaWxvbiIsImEiOiJjazZmNHA1bWoxNHoyM29td2k1MjVncm16In0.k99zSrB13Geh7G_bU-GZzw"}
-                    onViewportChange={(viewport) => this.setState({viewport})}
-                />
+                <Search/>
+                <Map />
             </div>
         )
     }
