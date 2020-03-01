a6582c870a31615f26a6c6cb6ffc0ce4df8dc735
Anton Kanugalawattage <antondilon@gmail.com>
2020-03-01 06:51:45 -0500

    destinations

---
+++ src/Components/Destinations.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Paper from '@material-ui/core/Paper';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+export class Destinations extends Component {
+    render() {
+        return (
+            <Paper component="form" style={{padding:'3px 20px 3px 20px', paddingBottom: this.props.coords.length? '33px':'3px', marginTop:'10px', textAlign:'left'}}>
+                <h4 style={{ fontSize:'15.5px'}}>Destination List</h4>
+            <div>
+
+                {this.props.coords.map((coord,index) => (
+                    <div key={index} style={{padding:'0px', height:'44px'}}>
+                        <span className="circle"  style={{ fontSize:'14.5px'}}>{index+1}</span>
+                        <h5 style={{display:'inline-block', fontSize:'14.5px'}}>{coord}</h5>
+                    </div>
+                ))}
+                </div>
+                </Paper>
+        )
+    }
+}
+
+export default Destinations
